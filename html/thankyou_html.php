44356073465270722c499108c6a7fcf37f97dc8e
laudd <laudd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-14 20:07:35 +0000

    Adding first version of Laudd Social Sharing plugin
    git-svn-id: https://plugins.svn.wordpress.org/laudd-social-sharing/trunk@1090069 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ html/thankyou_html.php
@@ -0,0 +1,60 @@
+
+<?php
+	if(strlen(get_option('laudd_siteid_old'))>0 && isset($_REQUEST['Reset-data']))
+	{
+			$laudd_registered = get_option('laudd_registered_old');
+			$laudd_domain 	  = get_option('laudd_domain_old');
+			$laudd_siteid 	  = get_option('laudd_siteid_old');
+			$laudd_fb		  = get_option('laudd_fb_old');
+			$laudd_tw		  = get_option('laudd_tw_old');
+			$laudd_li 		  = get_option('laudd_li_old');
+			$laudd_gp		  = get_option('laudd_gp_old');
+			
+			update_option('laudd_registered', $laudd_registered);
+			update_option('laudd_domain', $laudd_domain );
+			update_option('laudd_siteid', $laudd_siteid );
+			update_option('laudd_fb', $laudd_fb);
+			update_option('laudd_tw', $laudd_tw);
+			update_option('laudd_li', $laudd_li );
+			update_option('laudd_gp', $laudd_gp);
+			
+			delete_option('laudd_registered_old');
+			delete_option('laudd_domain_old');
+			delete_option('laudd_siteid_old');
+			delete_option('laudd_fb_old');
+			delete_option('laudd_tw_old');
+			delete_option('laudd_li_old');
+			delete_option('laudd_gp_old');
+			echo "<script>location.reload();</script>";
+	}
+?>
+<?php  if(strlen(get_option('laudd_siteid')) != 0 ){ ?>
+<input type="hidden" id="admin_url" value=""  />
+<input type="hidden" id="plugin_url" value="<?php echo plugins_url('../', __file__); ?>" />
+<div class="wrapper laudd-html">    
+    <div class="header">
+        <div class="container">
+            <div class="header-inner">
+                <div class="logo">
+                    <img src="<?php echo plugins_url( '../images/logo.png', __FILE__ ) ?>" alt="img">
+                </div>
+            </div>
+        </div>      
+    </div>  
+      <div class="content" style="margin-top:0px;">
+        <div class="container">
+                  <div class="l_success message" style="margin-bottom:100px;">
+					 <h3>Activation Successful!</h3>
+					 <p>The Laudd WordPress plugin is now active. The Laudd toolbar will now appear on all posts, and if you haven’t unset automatic premium content setting in the Laudd Publisher Portal, the bottom 25% (approximately) of each post will be marked premium.</p>
+					<p>To manually mark premium content, select content in the visual editor (WYSIWYG editor), and click the "Mark Premium Content" button.</p>			
+					<div class="btn-cont">
+						<a href="<?php echo admin_url( 'edit.php' ); ?>" class="thanks-btn">OK</a>
+					</div>
+						</div>
+				</div>
+           
+        </div>
+    </div> 
+</div>
+<!--------------------------------------------------------------------------------------------------------------->
+<?php  } ?>
