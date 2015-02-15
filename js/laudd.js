52d474115cc21fa467134feee74ce341e955bda5
laudd <laudd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-15 20:49:59 +0000

    added missing JS directory
    git-svn-id: https://plugins.svn.wordpress.org/laudd-social-sharing/tags/4.3.2@1090712 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/laudd.js
@@ -0,0 +1,44 @@
+
+/*--------- Laudd Jquery File----------------*/
+	function laudd_redirect(url)
+	{
+		location.assign(url);
+	}
+
+/*--------- Code for error Notification */
+	function showError(txt){
+		jQuery('.l_error p').text(txt);
+		jQuery('.l_error').slideDown();
+		setTimeout(function(){jQuery('.l_error').slideUp('slow')}, 3000);
+	}
+
+/*---------  Validation and for Laudd Registration*/
+	function validate()
+	{
+		var site_id = jQuery('#laudd_site_id').val();	
+		var redirect_page = jQuery('#thanks-redirect-page').val();	
+		if (site_id == ''){
+			showError("Please enter the Site ID.");
+			jQuery('#laudd_site_id').focus();
+			return false;
+		} else {
+			jQuery.ajax({
+				type: "POST",
+				url: ajaxurl,
+				data:"site_id=" + site_id + "&action=check_laudd_site_id_status",
+				beforeSend: function() {
+					jQuery('#submit_site_id').val('Loading...');
+				},
+				success: function(data) {
+					jQuery('#submit_site_id').val('Submit');
+					if( data == 1){
+						window.location.href= redirect_page; 
+					} else {
+						showError("Please enter the correct Site ID.");
+					}
+				}
+			});
+			return false;
+		}
+	}
+
